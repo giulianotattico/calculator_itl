9e23db8d8e244e39a9c328b2a3104936a847f440
GitKodland <125306926+GitKodland@users.noreply.github.com>
2023-06-01 16:19:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
-#Импорт
+# Import
 from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
 def result_calculate(size, lights, device):
-    #Переменные для энергозатратности приборов
+    # Variables that allow for the calculation of the appliances' energy draw
     home_coef = 100
     light_coef = 0.04
     devices_coef = 5   
     return size * home_coef + lights * light_coef + device * devices_coef 
 
-#Первая страница
+# The first page
 @app.route('/')
 def index():
     return render_template('index.html')
 
-#Вторая страница
+# The second page
 @app.route('/<size>')
 def lights(size):
     return render_template(
@@ -24,7 +24,7 @@ def lights(size):
                             size=size
                            )
 
-#Третья страница
+# The third page
 @app.route('/<size>/<lights>')
 def electronics(size, lights):
     return render_template(
