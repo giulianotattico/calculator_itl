7d8c016cbdf01d15859c5e9a4c089ed680b71466
GitKodland <125306926+GitKodland@users.noreply.github.com>
2023-11-02 15:10:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,18 +5,18 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 def result_calculate(size, lights, device):
-    # Variables that allow for the calculation of the appliances' energy draw
+    # Variabili che consentono di calcolare il consumo energetico degli apparecchi
     home_coef = 100
     light_coef = 0.04
     devices_coef = 5   
     return size * home_coef + lights * light_coef + device * devices_coef 
 
-# The first page
+# La prima pagina
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# The second page
+# La seconda pagina
 @app.route('/<size>')
 def lights(size):
     return render_template(
@@ -24,7 +24,7 @@ def lights(size):
                             size=size
                            )
 
-# The third page
+# La terza pagina
 @app.route('/<size>/<lights>')
 def electronics(size, lights):
     return render_template(
@@ -33,7 +33,7 @@ def electronics(size, lights):
                             lights = lights                           
                            )
 
-# Calculation
+# Calcolo
 @app.route('/<size>/<lights>/<device>')
 def end(size, lights, device):
     return render_template('end.html', 
